311ea89105c32df392ed828cd69886cdc85bcdff
Aliza Faisal <sp24-bse-048@cuiatk.edu.pk>
2026-08-25 01:20:24 +0500

    Update submit_complaint.php

--- Student/submit_complaint.php
+++ Student/submit_complaint.php
@@ -1,7 +1,10 @@
 <?php
 
 session_start();
-
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
+exit;
 include "../connection.php";
 // =========================================
 // AMS API CONFIGURATION
